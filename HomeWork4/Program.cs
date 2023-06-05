678e8232324c583b519efc654004ba0466ead36b
artem <artem28282828@gmail.com>
2023-06-05 15:21:30 +0500

    Задача два

---
+++ HomeWork4/Program.cs
@@ -0,0 +1,21 @@
+﻿int [] GetArray (int size , int start , int finish)
+{
+    int [] resultArrray = new int [size];
+    for (int i = 0 ; i < resultArrray.Length ; i++)
+    {
+        resultArrray[i] = new Random().Next(start , finish);
+    }
+    return resultArrray;
+}
+int [] array = GetArray(4 ,-100 , 100);
+Console.WriteLine($"В массиве: [ {String.Join("; ", array)}]");
+
+int numbSum = 0 ;
+for(int x = 0 ; x < array.Length ; x++)
+{
+    if(x % 2 != 0) 
+    {
+        numbSum += array[x];
+    }
+}
+Console.WriteLine($"сумма элементов стоящих на нечетных позициях {numbSum}");
